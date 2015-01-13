45ec44f455b49fdec3ea72fe9e49ef5e98e81925
Jeremy Fairbank <elpapapollo@gmail.com>
2015-01-13 13:57:02 -0500

    Add ability to output palette as an array of color format strings (#10)

--- lib/chroma/harmonies.rb
+++ lib/chroma/harmonies.rb
@@ -4,20 +4,20 @@ module Chroma
       @color = color
     end
 
-    def complement
-      [@color, @color.complement]
+    def complement(options = {})
+      with_reformat([@color, @color.complement], options[:as])
     end
 
-    def triad
-      hsl_map([0, 120, 240])
+    def triad(options = {})
+      hsl_map([0, 120, 240], options)
     end
 
-    def tetrad
-      hsl_map([0, 90, 180, 270])
+    def tetrad(options = {})
+      hsl_map([0, 90, 180, 270], options)
     end
 
-    def split_complement
-      hsl_map([0, 72, 216])
+    def split_complement(options = {})
+      hsl_map([0, 72, 216], options)
     end
 
     def analogous(options = {})
@@ -28,10 +28,12 @@ module Chroma
       part = 360 / slices
       hsl.h = ((hsl.h - (part * results >> 1)) + 720) % 360
 
-      (results - 1).times.reduce([@color]) do |arr, n|
+      palette = (results - 1).times.reduce([@color]) do |arr, n|
         hsl.h = (hsl.h + part) % 360
         arr << Color.new(hsl, @color.format)
       end
+
+      with_reformat(palette, options[:as])
     end
 
     def monochromatic(options = {})
@@ -40,21 +42,30 @@ module Chroma
       h, s, v = @color.hsv
       modification = 1.0 / results
 
-      results.times.map do
+      palette = results.times.map do
         Color.new(ColorModes::Hsv.new(h, s, v), @color.format).tap do
           v = (v + modification) % 1
         end
       end
+
+      with_reformat(palette, options[:as])
     end
 
     private
 
-    def hsl_map(degrees)
+    def with_reformat(palette, as)
+      palette.map! { |color| color.to_s(as) } unless as.nil?
+      palette
+    end
+
+    def hsl_map(degrees, options)
       h, s, l = @color.hsl
 
-      degrees.map do |deg|
+      degrees.map! do |deg|
         Color.new(ColorModes::Hsl.new((h + deg) % 360, s, l), @color.format)
       end
+
+      with_reformat(degrees, options[:as])
     end
   end
 end
