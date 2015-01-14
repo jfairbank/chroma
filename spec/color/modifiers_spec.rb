826f4f2f7924cbf8d1354b7d0c18cecb9e8e05c4
Jeremy Fairbank <elpapapollo@gmail.com>
2015-01-14 00:06:17 -0500

    Add remaining specs for Color modifier methods

--- spec/color/modifiers_spec.rb
+++ spec/color/modifiers_spec.rb
@@ -1,9 +1,87 @@
-require 'spec_helper'
-
 describe Chroma::Color do
+  let(:red)    { 'red'.paint }
+  let(:yellow) { 'yellow'.paint }
+
+  describe '#lighten' do
+    context 'with default amount' do
+      it 'generates the correct color' do
+        expect(red.lighten).to eq '#ff3333'.paint
+      end
+    end
+
+    context 'with supplied amount' do
+      it 'generates the correct color' do
+        expect(red.lighten(20)).to eq '#ff6666'.paint
+      end
+    end
+  end
+
+  describe '#brighten' do
+    context 'with default amount' do
+      it 'generates the correct color' do
+        expect(red.brighten).to eq '#ff1a1a'.paint
+      end
+    end
+
+    context 'with supplied amount' do
+      it 'generates the correct color' do
+        expect(red.brighten(20)).to eq '#ff3333'.paint
+      end
+    end
+  end
+
+  describe '#darken' do
+    context 'with default amount' do
+      it 'generates the correct color' do
+        expect(red.darken).to eq '#cc0000'.paint
+      end
+    end
+
+    context 'with supplied amount' do
+      it 'generates the correct color' do
+        expect(red.darken(20)).to eq '#990000'.paint
+      end
+    end
+  end
+
+  describe '#desaturate' do
+    context 'with default amount' do
+      it 'generates the correct color' do
+        expect(red.desaturate).to eq '#f20d0d'.paint
+      end
+    end
+
+    context 'with supplied amount' do
+      it 'generates the correct color' do
+        expect(red.desaturate(20)).to eq '#e61919'.paint
+      end
+    end
+  end
+
+  describe '#saturate' do
+    context 'with default amount' do
+      it 'generates the correct color' do
+        expect('#123'.paint.saturate).to eq '#0e2236'.paint
+      end
+    end
+
+    context 'with supplied amount' do
+      it 'generates the correct color' do
+        expect('#123'.paint.saturate(20)).to eq '#0a223a'.paint
+      end
+    end
+  end
+
+  describe '#grayscale' do
+    it 'generates the correct color' do
+      expect(red.grayscale).to           eq 'gray'.paint
+      expect('green'.paint.grayscale).to eq '#404040'.paint
+    end
+  end
+
   describe '#spin' do
     it 'generates the correct color' do
-      expect('red'.paint.spin(60)).to eq('yellow'.paint)
+      expect(red.spin(60)).to eq yellow
     end
   end
 end
